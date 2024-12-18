3025a366f179c180a925ac8d279a51db719f9f7a
BT-devv <buitrungtuan2003@gmail.com>
2024-12-18 20:07:49 +0700

    config code and add morgan

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); // Để sử dụng biến môi trường từ .env
+const mongoose = require("mongoose");
+require("dotenv").config(); // Để sử dụng biến môi trường từ .env
 
 // Kết nối MongoDB
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Database established successfully');
-    } catch (err) {
-        console.error('Database error code: ', err.message);
-        process.exit(1); // Thoát ứng dụng nếu kết nối thất bại
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database established successfully");
+  } catch (err) {
+    console.error("Database error code: ", err.message);
+    process.exit(1); // Thoát ứng dụng nếu kết nối thất bại
+  }
 };
 
 module.exports = connectDB;
