6ba2f5a5b2acdacd6cd3c2d48e175dd72397a779
NgKieu <ngkieu1811@gmail.com>
2025-03-11 22:11:59 +0700

    logout, API list attendance, update API OT/Absence

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -11,8 +11,8 @@ router.post("/check-in", checkIn); // Endpoint check-in
 router.post("/check-out", checkOut); // Endpoint check-out
 router.get("/", getListAttendances);
 /* 
-GET http://localhost:5000/api/attendances (full danh sách)
-GET http://localhost:5000/api/attendances?employeeID=EMP-2500001 (lọc theo EmployeeID)
-GET http://localhost:5000/api/attendances?date=2025-03-11 (lọc theo date)
+GET http://localhost:5000/api/attendance(full danh sách)
+GET http://localhost:5000/api/attendance/?employeeID=EMP-2500001 (lọc theo EmployeeID)
+GET http://localhost:5000/api/attendance/?date=2025-03-11 (lọc theo date)
 */
 module.exports = router;
